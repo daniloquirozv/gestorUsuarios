fa80fc6c2b67275c1725637bedc86f06b0ea4549
dan <dan.quirozv@duocuc.cl>
2025-08-24 13:59:36 -0400

    Model: Users

--- src/main/java/com/gestusers/gestorUsers/model/User.java
+++ src/main/java/com/gestusers/gestorUsers/model/User.java
@@ -1,16 +1,40 @@
 package com.gestusers.gestorUsers.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     
+    @Column(unique = true, nullable = false )
     private String username;
 
+    @Column(nullable = false)
     private String password;
+    
+    @Column(unique = true, nullable = false)
     
     private String email;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastName;
 
     private Boolean enables = true;
