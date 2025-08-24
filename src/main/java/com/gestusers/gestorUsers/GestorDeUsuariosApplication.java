5e411bca138c4fc14aa5d7c45e9788d66c093739
dan <dan.quirozv@duocuc.cl>
2025-08-23 22:14:52 -0400

    Init

---
+++ src/main/java/com/gestusers/gestorUsers/GestorDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.gestusers.gestorUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestusers/gestorUsers/GestorDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestusers.gestorUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
