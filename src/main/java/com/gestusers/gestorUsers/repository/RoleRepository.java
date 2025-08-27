2dcd7fd9a2c7c900da7b2005674827245a3d15cb
dan <dan.quirozv@duocuc.cl>
2025-08-27 16:00:10 -0400

    Repository DD

---
+++ src/main/java/com/gestusers/gestorUsers/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.gestusers.gestorUsers.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gestusers.gestorUsers.model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role.RoleName name);
+
+}
--- src/main/java/com/gestusers/gestorUsers/repository/UserRepository.java
+++ src/main/java/com/gestusers/gestorUsers/repository/UserRepository.java
@@ -1,10 +1,24 @@
 package com.gestusers.gestorUsers.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.gestusers.gestorUsers.model.Role;
 import com.gestusers.gestorUsers.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    
+import feign.Param;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existByUsername(String username);
+    Boolean existByEmail(String email);
+
+    @Query("Select u from User u join u.roles r where r.name = :rolename")
+    List<User> findByRoleName(@Param("rolename") Role.RoleName roleName);
 }
