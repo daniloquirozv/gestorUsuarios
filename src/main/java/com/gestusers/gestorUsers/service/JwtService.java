14d2c32d8fd20a761cae37fba56635badf6ebedf
dan <dan.quirozv@duocuc.cl>
2025-09-01 21:50:18 -0400

    Web Token Done

--- src/main/java/com/gestusers/gestorUsers/service/JwtService.java
+++ src/main/java/com/gestusers/gestorUsers/service/JwtService.java
@@ -4,12 +4,14 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -38,6 +40,39 @@ public class JwtService {
                 .signWith(Keys.hmacShaKeyFor(JWT_SECRET.getBytes()), Jwts.SIG.HS512)
                 .compact();
     }
+
+    public Claims getAllClaimsFromToken(String token){
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(JWT_SECRET.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String getUsernameFromToken(String token){
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token){
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token) );
+    }
+
+
 
     
 }
